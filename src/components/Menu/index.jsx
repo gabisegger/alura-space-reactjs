ee118bffbd4a69c6ceacfc9781f1b8a2920ff505
gabisegger <gabi.segger@gmail.com>
2023-05-24 19:47:36 -0300

    Criação do Menu

---
+++ src/components/Menu/index.jsx
@@ -0,0 +1,35 @@
+import home from '../../assets/icones/home.png';
+import maisCurtidas from '../../assets/icones/mais-curtidas.png';
+import maisVistas from '../../assets/icones/mais-vistas.png';
+import novas from '../../assets/icones/novas.png';
+import surpreenda from '../../assets/icones/surpreenda.png';
+import styles from './Menu.module.scss';
+
+export default function Menu() {
+  return (
+    <nav className={styles.menu}>
+        <ul className={styles.menu__list}>
+            <li className={styles.menu__item}>
+                <img src={home} alt="" />
+                <a href="/">Início</a>
+            </li>
+            <li className={styles.menu__item}>
+                <img src={maisCurtidas} alt="" />
+                <a href="/">Mais curtidas</a>
+            </li>
+            <li className={styles.menu__item}>
+                <img src={maisVistas} alt="" />
+                <a href="/">Mais vistas</a>
+            </li>
+            <li className={styles.menu__item}>
+                <img src={novas} alt="" />
+                <a href="/">Novas</a>
+            </li>
+            <li className={styles.menu__item}>
+                <img src={surpreenda} alt="" />
+                <a href="/">Surpreenda-me</a>
+            </li>
+        </ul>
+    </nav>
+  )
+}
