8d1162b08b173e4346198bff6618a5369e333628
gabisegger <gabi.segger@gmail.com>
2023-05-29 06:58:15 -0300

    Filter feature

--- src/components/Gallery/index.jsx
+++ src/components/Gallery/index.jsx
@@ -2,13 +2,26 @@ import Tags from "../Tags";
 import Cards from "./Cards";
 import photos from "./photos.json";
 import styles from "./Gallery.module.scss";
+import { useState } from "react";
 
 export default function Gallery() {
+  
+  const [itens, setItens] = useState(photos);
+  const tags = [...new Set(photos.map((valor) => valor.tag))]
+
+  const filtraFotos = (tag) => {
+    const newPhotos = photos.filter((photo) => {
+      return photo.tag === tag;
+    })
+
+    setItens(newPhotos);
+  }
+
   return (
     <section className={styles.gallery}>
         <h2>Navegue pela galeria</h2>
-        <Tags />
-        <Cards itens={photos} styles={styles}/>
+        <Tags tags={tags} filtraFotos={filtraFotos} setItens={setItens}/>
+        <Cards itens={itens} styles={styles}/>
     </section>
   )
 }
--- src/components/Tags/index.jsx
+++ src/components/Tags/index.jsx
@@ -1,14 +1,15 @@
 import styles from './Tags.module.scss';
+import photos from "../Gallery/photos.json";
 
-export default function Tags() {
+export default function Tags({ tags, filtraFotos, setItens }) {
   return (
     <div className={styles.tags}>
         <p>Filtre por tags:</p>
         <ul className={styles.tags__list}>
-            <li>Estrelas</li>
-            <li>Galáxias</li>
-            <li>Lua</li>
-            <li>Planetas</li>
+          {tags.map((tag) => {
+            return <li key={tag} onClick={() => filtraFotos(tag)}>{tag}</li>;
+          })}
+          <li onClick={()=>setItens(photos)}>Todas</li>
         </ul>
     </div>
   )
